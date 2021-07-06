9abc10ba27562df207fd8a3fe71acd176a297513
André Roberge <andre.roberge@gmail.com>
2021-07-06 05:48:02 -0300

    updating some batch files

--- check.bat
+++ check.bat
@@ -1,11 +1,11 @@
+
+call venv-friendly-traceback-3.8\scripts\activate
 cd tests
-
-call ..\venv-friendly-traceback-3.8\scripts\activate
 call python trb_english.py
-call  python trb_syntax_english.py
-call  deactivate
+call python trb_syntax_english.py
+call deactivate
 
-cd ..\..\friendly-traceback-docs\docs
+cd ..\..\docs
 call make html
-cd ..\..\friendly_traceback
+cd ..\friendly-traceback
 call venv-friendly-traceback-3.8\scripts\activate
--- upgrade_all.bat
+++ upgrade_all.bat
@@ -1,24 +1,26 @@
 call venv-friendly-traceback-3.6\scripts\activate
 call python -m pip install -r requirements.txt --upgrade
 call python -m pip install -r requirements-dev.txt --upgrade
-
+call python -m pip install pip --upgrade
 
 call venv-friendly-traceback-3.7\scripts\activate
 call python -m pip install -r requirements.txt --upgrade
 call python -m pip install -r requirements-dev.txt --upgrade
-
+call python -m pip install pip --upgrade
 
 call venv-friendly-traceback-3.8\scripts\activate
 call python -m pip install -r requirements.txt --upgrade
 call python -m pip install -r requirements-dev.txt --upgrade
-
+call python -m pip install pip --upgrade
 
 call venv-friendly-traceback-3.9\scripts\activate
 call python -m pip install -r requirements.txt --upgrade
 call python -m pip install -r requirements-dev.txt --upgrade
+call python -m pip install pip --upgrade
 
 call venv-friendly-traceback-3.10\scripts\activate
 call python -m pip install -r requirements.txt --upgrade
 call python -m pip install -r requirements-dev.txt --upgrade
+call python -m pip install pip --upgrade
 
 call venv-friendly-traceback-3.9\scripts\activate
