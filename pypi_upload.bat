e35118ede817d0bc00b58849943dc827800f0df7
André Roberge <andre.roberge@gmail.com>
2021-07-25 07:21:00 -0300

    Changing bat files to ensure that a clean build is done prior to uploading to pypi. See Issue #24

--- pypi_upload.bat
+++ pypi_upload.bat
@@ -15,7 +15,9 @@ If /I "%ANSWER%"=="y" goto yes
 If /I "%ANSWER%"=="n" goto no
 echo Incorrect input & goto Ask2
 :yes
-del /Q dist\*.*
+rd /S /Q dist
+rd /S /Q build
+rd /S /Q __pycache__
 python setup.py sdist bdist_wheel
 twine upload dist/*
 :no
