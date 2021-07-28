1320cf6c5da8fe759b8aa5ccd4e5a50467a92c96
oleg.hoefling <oleg.hoefling@gmail.com>
2021-07-28 08:29:13 +0200

    add type hints for friendly_traceback.debug_helper
    Signed-off-by: oleg.hoefling <oleg.hoefling@gmail.com>

--- friendly_traceback/debug_helper.py
+++ friendly_traceback/debug_helper.py
@@ -6,8 +6,9 @@ The idea is to silence internal exceptions raised by Friendly
 itself for most users by redirecting them here, and have them
 printed only when debugging mode is activated.
 """
-import sys
 import os
+import sys
+from typing import Any, Optional
 
 # DEBUG is set to True for me. It can also be set to True from __main__ or when
 # using the debug() command in the console.
@@ -21,12 +22,12 @@ DEBUG = IS_PYDEV or IS_ANDRE
 EXIT = False
 
 
-def log(text):
+def log(text: Any) -> None:
     if DEBUG:  # pragma: no cover
         print(text)
 
 
-def log_error(exc=None):
+def log_error(exc: Optional[BaseException] = None) -> None:
     global EXIT
     if DEBUG:  # pragma: no cover
         from . import explain_traceback
