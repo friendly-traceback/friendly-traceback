76247081bfb46d16eaf63cab59f805ddfa8b60fa
André Roberge <andre.roberge@gmail.com>
2021-12-19 21:12:46 -0400

    Adding unit tests for issue #98

---
+++ tests/runtime/test_runtime_error.py
@@ -0,0 +1,27 @@
+import friendly_traceback as ft
+
+
+def test_size_changed_during_iteration():
+    squares = {1:1, 2:4, 3:9}
+    try:
+        for square in squares:
+            squares.pop(square)
+    except RuntimeError as e:
+        ft.explain_traceback(redirect="capture")
+    result = ft.get_output()
+    assert "RuntimeError" in result
+    if ft.get_lang() == "en":
+        assert "While you were iterating over the items of `squares` (a dictionary (`dict`))" in result
+
+    numbers = {1, 2, 3}
+    try:
+        for n in numbers:
+            numbers.remove(n)
+    except RuntimeError as e:
+        message = str(e)
+        ft.explain_traceback(redirect="capture")
+    result = ft.get_output()
+    assert "RuntimeError" in result
+    if ft.get_lang() == "en":
+        assert "While you were iterating over the items of `numbers` (a `set`)" in result
+    return result, message
