38676a19229993e585f2c777935a8a93a8e84271
André Roberge <andre.roberge@gmail.com>
2022-02-04 12:16:59 -0400

    Adding test for issue #118

---
+++ tests/unit/test_source_missing.py
@@ -0,0 +1,27 @@
+import friendly_traceback
+
+def test_With_exec():
+    try:
+        exec("1/0")
+    except ZeroDivisionError:
+        friendly_traceback.explain_traceback(redirect="capture")
+    result = friendly_traceback.get_output()
+
+    if friendly_traceback.get_lang() == "en":
+        assert "<string> is not a regular Python file" in result
+    else:
+        assert "<string>" in result
+
+
+def test_With_fake_file():
+    code = compile("1/0", "<fake>", "exec")
+    try:
+        exec(code)
+    except ZeroDivisionError:
+        friendly_traceback.explain_traceback(redirect="capture")
+    result = friendly_traceback.get_output()
+
+    assert "<fake>" in result
+    if friendly_traceback.get_lang() == "en":
+        assert "The following mathematical expression includes a division by zero" in result
+        assert "Internal error" not in result
