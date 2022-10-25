5503f5aa0d92e1f12cfed43be56aa88962f869ae
André Roberge <andre.roberge@gmail.com>
2022-10-25 13:14:53 -0300

    Adding 3.12 to test suite (excluding docs)

--- run_all_tests.bat
+++ run_all_tests.bat
@@ -1,4 +1,4 @@
-
+echo Python 3.6:
 call venv-friendly-traceback-3.6\scripts\activate & python -m pytest
 
 echo Python 3.7:
@@ -14,6 +14,9 @@ echo Python 3.10:
 call venv-friendly-traceback-3.10\scripts\activate & python -m pytest
 
 echo Python 3.11:
-call venv-friendly-traceback-3.10\scripts\activate & python -m pytest
+call venv-friendly-traceback-3.11\scripts\activate & python -m pytest
 
-call venv-friendly-traceback-3.9\scripts\activate
+echo Python 3.12:
+call venv-friendly-traceback-3.12\scripts\activate & python -m pytest
+
+call venv-friendly-traceback-3.10\scripts\activate
